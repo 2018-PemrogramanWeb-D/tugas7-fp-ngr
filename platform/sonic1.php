8f66c9af71388aaf1bf114afe492b4617d445db4
Neorocket64 <neorocket64@gmail.com>
2018-11-01 11:47:24 +0700

    review game

---
+++ platform/sonic1.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+
+<html lang="id" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../CSS.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+    <link rel="icon" href="logo.ico">
+	<title>NGR - Nice Game Review</title>
+ <style>
+
+	img{
+		width:250px;
+		height:150px;
+	}
+
+</style>
+<body>
+	<div class="container-top">
+		<?php include '../header.php';?>
+	</div>
+
+
+	<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
+		<?php include '../navbar.php';?>
+	</nav>
+	<div class="container">	
+			<br>
+		<table class="table table-striped">
+			<thead class="thead-dark">
+				<tr>
+					<th colspan="3">Sonic the Hedgehog (1991)</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td rowspan="6"><img src="http://info.sonicretro.org/images/thumb/3/37/Sonic1_box_jap.jpg/800px-Sonic1_box_jap.jpg" /></a></td>
+				</tr>
+				<tr><td>Nama</td><td>Sonic the Hedgehog</td></tr>
+				<tr><td>Developer</td><td>Sonic Team</td></tr>
+				<tr><td>publisher</td><td>SEGA</td></tr>
+				<tr><td>Platform</td><td>Megadrive/Genesis</td></tr>
+				<tr><td>Release Date</td><td>1991</td></tr>
+			</tbody>
+		</table>
+        <div style="text-align:center;"><img src="http://retrocdn.net/images/6/6f/Sonic1_title.png" alt="Title Screen"></div>
+		<p>
+            pada tahun 1991, singkat cerita SEGA ingin menandingi console nintendo dan franchise Mario milik mereka dengan Sonic.<br>
+            Apakah hasilnya?<br>
+            Bermain sebuah permainan platform dengan kecepatan tinggi dengan teknologi saat itu terdengar menarik, tapi apakah sesuai ekspektasi?<br>
+        </p>
+        <h3>Permainan</h3>
+        <p>
+            Kontrol untuk bermain ini cukup simpel, 1 tombol dan 1 tombol arah (kanan kiri). Pemain akan mengontrol Sonic (landak biru) yang menggunakan kecepatan sebagai bagian utama dalam permainan ini.<br>
+            Memanfaatkan kecepatan dan gravitasi adalah kunci untuk menyelesaikan permainan ini!<br>
+            Stage dalam permainan ini berjumlah 6 dan setiap stage ada 3 level yang disebut "ACT" diikuti dengan Final Stage.<br>
+            Setiap stage mempunyai tema dan setiap akhir dari stage akan diberikan Boss.<br>
+            <img src="http://info.sonicretro.org/images/0/07/Ghzboss.png" alt="Melawan Boss!"><br>
+            Awalnya, yang anda lihat saat bermain stage pertama memberi kesan yang sesuai ekspektasi.<br>
+            berlari cepat mengejar waktu menuju tujuan. Tapi setelah itu, secara cepat anda sadari, stage selanjutny lebih menuju "tricky platforming",
+            dimana anda harus mengatur waktu anda untuk melompat dan menghindari berbagai hal yang berbahaya.<br>
+            Terlihat tidak mengganggu, tetapi untuk permainan yang seharusnya "cepat", ini sedikit keluar dari tujuan mereka<br>
+            <img src="http://info.sonicretro.org/images/8/82/Marble.png" alt="Lava???"><br>
+            <b>Bagaimana dengan level designnya sendiri?</b><br>
+            Menarik! Bisa dibilang itu, karena setiap stage mempunyai tantangan tersendiri. Setiap "obstacle" yang dimiliki setiap stage berbeda dengan lain,
+             dengan kata lain, tidak repetitif<br>
+             Untuk stage biasa sudah bagus, tapi ada stage yang spesial untuk mendapatkan bonus skor! Tapi, lihatlah ini;<br>
+             <img src="http://info.sonicretro.org/images/1/1c/Specialstages1.png" alt="Special Stage"><br>
+             Jika anda sedikit pusing melihatnya, itu tidak salah anda, karena stage ini memang memusingkan.<br>
+             Stage pun dirancang sedemikian rupa agar memanfaatkan gravitasi dan kecepatan dalam hal yang berbeda.<br>
+             Area akan berputar selama anda masih dalam stage tersebut dan anda dalam kondisi "lompat".<br>
+             Tujuan dalam special stage ini adalah untuk mendapatkan bonus item, Chaos Emerald!<br>
+             Meskipun itu, Chaos Emerald tidak lain hanyalah untuk mendapatkan ending terbaik.<br>
+             Ada sebuah kristal yang menghalangi anda menuju Chaos Emerald! anda bisa menghilangkannya seperti memecahkan kaca, sedikit demi sedikit akan pecah
+             yang akan ditandai dengan warna kristal tersebut.<br>
+             Terlihat simpel? Ya! Oleh karena itu, stagenya susah untuk dilakukan dalam 1 waktu main. Tidak hanya karena ada area yang mengirim anda menuju stage selanjutnya (tanpa mendapatkan Chaos Emerald),
+             tetapi pemain juga diberi sebuah "Bumper" yang memantulkan Sonic, tombol untuk mempercepat dan memutar arah lain lintasan, dan lain sebagainya.<br>
+        </p>
+        <h3>Aestetis</h3>
+        <p>
+        <img src="http://info.sonicretro.org/images/1/1b/Ghz.png" alt="Green Hill Zone">
+        <img src="http://info.sonicretro.org/images/4/4e/Labyrinth.png" alt="Labyrinth Zone">
+        <img src="http://info.sonicretro.org/images/7/7b/Sonic1_ScrapBrainAct1.png" alt="Scrap Brain Zone">
+        <img src="http://info.sonicretro.org/images/1/15/Starlight.png" alt="Star Light Zone"><br>
+        Untuk tahun 1991, semua grafis bagus dan kontras. Setiap stage memiliki keunikan tersendiri.<br>
+        </p>
+        <h3>Suara</h3>
+        <p>
+            Dengan menggunakan keterbatasan soundchip yang berada dalam console tersebut, Lagu terdengar unik.<br>
+            Sonic Team telah menggambarkan suasana setiap stage dengan cukup baik.<br>
+            Tentu, efek suara yang dibuat untuk permainan ini terdengar unik dan jelas. Anda akan tahu suatu bahaya dan lain dengan mendengarkan suara tesebut.<br>
+            <iframe width="478" height="269" src="https://www.youtube.com/embed/xPRG1Fpp00U?list=PL6akIKaXBeU3ZcOnOYpRBl94Q-brvs_cs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </p>
+		
+		<table  class="table table-striped">
+			<thead class="thead-dark">
+				<tr>
+					<th colspan="3">Verdict</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td rowspan="4"><h1>7</h1></td>
+				</tr>
+                <tr><td>GamePlay</td><td>7/10</td></tr>
+				<tr><td>Graphics</td><td>7/10</td></tr>
+                <tr><td>Sounds</td><td>8/10</td></tr>
+			</tbody>
+		</table>
+		<br>
+	</div>
+	<br>
+	<div class="container">
+		<h3>Komentar</h3>
+		<form>
+			<input type="text" name="komentar" title="komentar" />
+			<input type="submit" value="click" />
+		</form>
+		<br>
+	</div>
+</body>
+</html>
