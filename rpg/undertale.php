1da2a5610cb9b38f90ac7f8c9d1b4e14c4e7dcac
Neorocket64 <neorocket64@gmail.com>
2018-11-07 14:49:38 +0700

    review stuff

---
+++ rpg/undertale.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+
+<html lang="id" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../CSS.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+    <link rel="icon" href="logo.ico">
+	<title>NGR - Nice Game Review</title>
+ <style>
+
+	img{
+		width:250px;
+		height:150px;
+	}
+
+</style>
+<body>
+	<div class="container-top">
+		<?php include '../header.php';?>
+	</div>
+
+
+	<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
+		<?php include '../navbar.php';?>
+	</nav>
+	<div class="container">	
+			<br>
+		<table class="table table-striped">
+			<thead class="thead-dark">
+				<tr>
+					<th colspan="3">Undertale</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td rowspan="5"><img src="https://yt3.ggpht.com/aXRtwPwqQ8yYDZ6e8EtNX8h9U_vztz4VOkgLdFKa97EfIJfXnwghFwxw1Pex4Fvr7mGWNY1ccc69LFXv_A=w390-h540-nd" alt="Undertale" /></a></td>
+				</tr>
+				<tr><td>Developer</td><td>Toby Fox</td></tr>
+				<tr><td>Publisher</td><td>Toby Fox</td></tr>
+				<tr><td>Platform</td><td>PC, Switch, PS Vita, PS4</td></tr>
+				<tr><td>Release Date</td><td>2015</td></tr>
+			</tbody>
+		</table>
+        <div style="text-align:center;"><img src="https://steamcdn-a.akamaihd.net/steam/apps/391540/header.jpg" alt="Title Screen"></div>
+		<p>
+            Pada 2015, seorang pembuat bernama Toby Fox, telah meriliskan Undertale dari keberhasilan Kick Starternya.<br>
+            Dengan bantuan teman-teman dan suporternya, game tersebut bisa diterima sangat baik dan terkenal seluruh dunia.<br>
+            Tetapi, apakah itu berarti game tersebut termasuk game yang bagus dalam sisi designnya?<br>
+        </p>
+        <h3>Cerita</h3>
+        <p>
+            Singkat cerita, seorang anak kecil bernama Frisk jatuh ke dalam gua dan gua tersebut berisikan para monster yang disegel oleh golongan manusia.<br>
+            Fokus dalam cerita ini adalah persahabatan dan "sparing".<br>
+            Secara singkat, cerita ini kurang bagus dan ada beberapa kekurangan, salah satunya adalah "exposition dumping".<br>
+            Karakter yang diberikan unik-unik dan menarik (interaksi dan dialog), akan tetapi, beberapa kurang "character developement" dan disengaja dibuat misterius.<br>
+            Game ini juga terdapat "4th wall breaking".<br>
+            <div style="margin: 5px 20px 20px;">
+            <div class="smallfont" style="margin-bottom: 2px;">
+                <b>Pendapat cerita (Spoiler)</b>:
+                <input value="Buka" style="margin: 0px; padding: 0px; width: 55px; font-size: 12px;" onclick="if (this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display != '') { this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = ''; this.innerText = ''; this.value = 'Tutup'; } else { this.parentNode.parentNode.getElementsByTagName('div')[1].getElementsByTagName('div')[0].style.display = 'none'; this.innerText = ''; this.value = 'Buka'; }" type="button">
+            </div>
+            <div class="alt2" style="border: 1px inset ; margin: 0px; padding: 6px;">
+            <div style="display: none;">
+                Cerita ini dibagi menjadi 3 rute, Pacifist, Normal, dan Genocide.<br>
+                <b>Normal</b><br>
+                Diekspektasi oleh creatornya sendiri untuk melewati cerita ini terlebih dahulu.<br>
+                Perjalanan Frisk sampai tujuan utama (mengalahkan Asgore) disertai dengan pengenalan karakter-karakter.<br>
+                Pada saat terakhir, anda akan bertemu dengan boss Flowey, dimana setelah itu, anda akan disuruh untuk mencoba rute Pacifist.<br>
+                <b>Pacifist</b><br>
+                Ini adalah rute yang sesungguhnya, dimana anda mencoba untuk tidak membunuh monster.<br>
+                Rute ini hampir sama dengan Normal, tetapi ditambah beberapa cerita tambahan dan beda ending.<br>
+                Cerita asal mengapa monster disegel dan beberapa detil lainny dijelaskan di sini... dengan cara yang kurang benar.<br>
+                Exposition dumping (cara pemasukan cerita secara tersurat (aka banyak teks seperti ini)) digunakan untuk penjelasan detil tersebut.<br>
+                <img src="https://vignette.wikia.nocookie.net/undertale/images/5/54/True2.png/revision/latest?cb=20160211165256" alt="secret lab"><br>
+                Semua cerita awal mula insiden tersimpan dalam monitor-monitor<br>
+                Meskipun ini cara termudah untuk memberikan narasi dalam cerita, namun hal ini juga bisa didapatkan di buku-buku cerita.<br>
+                Jadi, kurang kreatif dan memberi kesan malas.<br>
+                <b>Genocide</b><br>
+                Genocide menceritakan tentang Chara, karakter yang disebut mempunyai hubungan dengan insiden perang monster dan manusia.<br>
+                Dengan membunuh banyak monster, lama kelamaan (dalam ceritanya) Frisk akan dirasuki oleh Chara.<br>
+                Kemudian, Frisk akan melawan banyak boss.<br>    
+            </div>
+            </div>
+            </div>
+
+        </p>
+        <h3>Permainan</h3>
+        <p>
+            <img src="http://cdn.themis-media.com/media/global/images/library/deriv/992/992777.jpg" alt="bullet"><br>
+            Menggunakan turn-based rpg sebagai dasarnya, anda akan menghindar dari serangan musuh dengan cara yang unik.<br>
+            Apakah ini bagus?<br>
+            Pada awal, ini menarik. Karena setiap musuh akan menyerang dengan pola dan tampilan serangan yang berbeda.<br>
+            Namun, setelah beberapa kali bertemu dan menyerang, ada sisi repetitif yang mengganggu.<br>
+            Repetitif ini diakibatkan oleh pola cara mengalahkan monster yang sama dengan beberapa pilihan berbeda.<br>
+        </p>
+        <h3>Aestetis</h3>
+        <p>
+            <img src="http://www.i2i.gi/backup.php?getimage=aHR0cDovL3d3dy5ib2dsZWVjaC5jb20vaGFsbG93ZWVuL3VuZGVydGFsZS1wbGFudC5wbmc=" alt="pot">
+            <img src="https://anigamers.com/uploads/entries/Snapshot_UndertaleTutu_20160116011618.png" alt="papy">
+            <img src="http://dslv9ilpbe7p1.cloudfront.net/e1uxcMvelQ2YIKtHdqyPTg_store_header_image" alt="castle"><br>
+            Lebih pixelated dan warna simpel, aestetis yang didapatkan sangat kontras meskipun tempat utama permainan ini adalah gua.<br>
+            Saat posisi menyerang, musuh anda menggunakan warna hitam putih.<br>
+            <img src="https://vignette.wikia.nocookie.net/undertale/images/1/13/White_and_Green_attacks.png" alt="pesawat"><br>
+        </p>
+        <h3>Suara</h3>
+        <p>
+            Musik yang dibuat oleh Toby Fox sangat menakjubkan!<br>
+            Efek suara juga bagus dan jelas.<br>
+            <iframe width="478" height="269" src="https://www.youtube.com/embed/YTy9v9a7Tmo?list=PLoq58GDeKElcdxUImxXYKqG91vdxzwrBH" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>
+        </p>
+		
+		<table  class="table table-striped">
+			<thead class="thead-dark">
+				<tr>
+					<th colspan="3">Verdict</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td rowspan="5"><h1>7</h1></td>
+				</tr>
+                <tr><td>Story</td><td>6/10</td></tr>
+                <tr><td>Gameplay</td><td>7/10</td></tr>
+				<tr><td>Graphics</td><td>8/10</td></tr>
+                <tr><td>Sounds</td><td>9/10</td></tr>
+			</tbody>
+		</table>
+		<br>
+	</div>
+	<br>
+	<div class="container">
+		<h3>Komentar</h3>
+		<form>
+			<input type="text" name="komentar" title="komentar" />
+			<input type="submit" value="click" />
+		</form>
+		<br>
+	</div>
+</body>
+</html>
