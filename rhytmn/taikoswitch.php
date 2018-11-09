e5964159e4bd4066d7d7c013196953dd7266eb71
Neorocket64 <neorocket64@gmail.com>
2018-11-09 11:19:50 +0700

    review taiko

---
+++ rhytmn/taikoswitch.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+
+<html lang="id" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../CSS.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+    <link rel="icon" href="logo.ico">
+	<title>NGR - Nice Game Review</title>
+ <style>
+
+	img{
+		width:250px;
+		height:150px;
+	}
+
+</style>
+<body>
+	<div class="container-top">
+		<?php include '../header.php';?>
+	</div>
+
+
+	<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
+		<?php include '../navbar.php';?>
+	</nav>
+	<div class="container">	
+			<br>
+		<table class="table table-striped">
+			<thead class="thead-dark">
+				<tr>
+					<th colspan="3">Taiko no Tatsujin ~Nintendo Switch Version~</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td rowspan="5"><img src="https://nintendosoup.com/wp-content/uploads/2018/03/taiko-no-tatsujin-nintendo-switch-version-mar92018-1.jpg" alt="main" /></a></td>
+				</tr>
+				<tr><td>Developer</td><td>Bandai Namco</td></tr>
+				<tr><td>Publisher</td><td>Bandai Namco</td></tr>
+				<tr><td>Platform</td><td>Switch</td></tr>
+				<tr><td>Release Date</td><td>2018</td></tr>
+			</tbody>
+		</table>
+        <div style="text-align:center;"><img src="https://vignette.wikia.nocookie.net/taiko/images/5/56/TaikoSwitch1Logo.png" alt="Title Screen"></div>
+		<p>
+            Namco (sekarang Namco Bandai) membuat permainan rhythm dengan menggunakan alat musik tradisional, yaitu Drum.<br>
+            Meskipun terlihat simpel, namun banyak yang menyukainya dan laris sampai sekarang.<br>
+            Yang saya mainkan ini adalah versi Nintendo Switch. Beberapa lagu masuk dalam versi console agar versi arcadenya bisa tetap laris.<br>
+            Dan Versi Region yang saya mainkan adalah region Jepang dengan English Patch. Untuk worldwide, akan keluar akhir tahun ini (2018)<br>
+        </p>
+        <h3>Permainan</h3>
+        <p>
+            <img src="https://gematsu.com/wp-content/uploads/2018/07/Taiko-Switch-Rating_07-03-18.jpg" alt="Bermain!"><br>
+            Bermain irama dengan drum!<br>
+            Siap-siap lengan anda, karena ini bisa menguras banyak tenaga.<br>
+            Kontrol untuk bermain ini ada 4 cara, Motion, Touch, Tombol, dan Real (semua nama ini dikarang)<br>
+            <h4>Motion</h4>
+            <img src="https://gematsu.com/wp-content/uploads/2018/04/Taiko-Drum-Master-Nintendo-Switch-Version_2018_04-19-18_004.jpg" alt="Motion"><br>
+            Dengan menggunakan fitur motion dari joycon Nintendo Switch, Andaikan anda memukul drum asli.<br>
+            Memukul depan untuk tengah (merah), dan memukul samping untuk luar/rim (biru).<br>
+            <h4>Touch</h4>
+            <img src="http://www.siliconera.com/wordpress/wp-content/uploads/2018/07/taikoswitch4_thumb.png" alt="Touch"><br>
+            Menggunakan touch screen dari Switchny sendiri (portable mode), serasa bermain di touch smartphone.<br>
+            Tekan gambar drum untuk tengah (merah), dan luar drum untuk luar/rim (biru).<br>
+            <h4>Tombol</h4>
+            Menggunakan tombol dalam controller<br>
+            <h4>Real</h4>
+            <img src="http://cdn.shopify.com/s/files/1/0404/2733/products/IMG_4761_5debe2ca-339d-492d-8868-847a1ce0c816_grande.JPG" alt="Drummin"><br>
+            Menggunakan aksesoris berupa drum yang disediakan oleh HORI, seharga Rp 1.300.000 (Sumber : Tokopedia, 11/9/2018 10:49 WIB)<br><br>
+            Menggunakan drum adalah terbaik meskipun saya sendiri belum memilikinya (mau beli kapan-kapan), tetapi Motion juga tidak buruk (namun perlu pembiasaan)<br>
+            Masalah yang biasa dialami saat Motion adalah terkadang tidak responsif. Ini bisa diakibatkan oleh joycon sendiri.<br>
+            Tombol dan Touch responsif, dan cara itu adalah preferensi masing-masing jika tidak ingin memukul.<br><br>
+            <img src="https://i0.wp.com/www.play-asia.com/blog/wp-content/uploads/2018/08/Track-Lit.jpg" alt="Track List"><br>
+            Terlihat sedikit, namun ada DLC tersedia dalam e-shop.<br>
+            Tambahan lain untuk permainan ini adalah party mode untuk 2-4 player.<br>
+            Untuk seharga 680-800rb (varietas dari toko-toko), ini cukup memuaskan.<br>
+        </p>
+        <h3>Aestetis</h3>
+        <p>
+        <img src="https://nintendosoup.com/wp-content/uploads/2018/08/taiko-no-tatsujin-switch-vocaloid-pack-1-1038x576.jpg" alt="Miku">
+        <img src="https://i.kinja-img.com/gawker-media/image/upload/s--hoGFspUi--/c_scale,f_auto,fl_progressive,q_80,w_800/jnh8izt3tyfsyoqeswuz.jpg" alt="Masih Bermain"><br>
+        Aestetisnya bisa dibilang terlihat seperti untuk anak kecil.<br>
+        Namun, dengan itu, daya tarik untuk permainan ini ditujukan untuk segala usia.<br>
+        Yang harus diperhatikan untuk permainan rhythm adalah adalah kejelasan not apa yang harus dipukul, score, dsb.<br>
+        Dan permainan ini, seperti semua versi, sangat jelas dan mudah dibaca.<br>
+        </p>
+        <h3>Suara</h3>
+        <p>
+            Pukulan drum dalam permainan sangat memuaskan dan untuk tengah dan luar/rim dibedakan untuk mengetahui perbedaannya.<br>
+        </p>
+		
+		<table  class="table table-striped">
+			<thead class="thead-dark">
+				<tr>
+					<th colspan="3">Verdict</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td rowspan="4"><h1>8</h1></td>
+				</tr>
+                <tr><td>GamePlay</td><td>8/10</td></tr>
+				<tr><td>Graphics</td><td>9/10</td></tr>
+                <tr><td>Sounds</td><td>8/10</td></tr>
+			</tbody>
+		</table>
+		<br>
+	</div>
+	<br>
+	<div class="container">
+		<h3>Komentar</h3>
+		<form>
+			<input type="text" name="komentar" title="komentar" />
+			<input type="submit" value="click" />
+		</form>
+		<br>
+	</div>
+</body>
+</html>
