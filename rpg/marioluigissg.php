85d4b43e29dcc7a6fdd8eba7e72ebe87365b54b5
Neorocket64 <neorocket64@gmail.com>
2018-11-19 22:04:49 +0700

    reviews and prob. content

---
+++ rpg/marioluigissg.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+
+<html lang="id" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../CSS.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+    <link rel="icon" href="logo.ico">
+	<title>NGR - Nice Game Review</title>
+ <style>
+
+	img{
+		width:250px;
+		height:150px;
+	}
+
+</style>
+<body>
+	<div class="container-top">
+		<?php include '../header.php';?>
+	</div>
+
+
+	<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
+		<?php include '../navbar.php';?>
+	</nav>
+	<div class="container">	
+			<br>
+		<table class="table table-striped">
+			<thead class="thead-dark">
+				<tr>
+					<th colspan="3">Mario and Luigi Superstar Saga</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td rowspan="5"><img src="https://upload.wikimedia.org/wikipedia/en/6/63/MarioAndLuigiSuperstarSagaGBACoverArtUS.jpg" alt="Superstar Saga" /></a></td>
+				</tr>
+				<tr><td>Developer</td><td>AlphaDream</td></tr>
+				<tr><td>Publisher</td><td>Nintendo</td></tr>
+				<tr><td>Platform</td><td>GBA</td></tr>
+				<tr><td>Release Date</td><td>2003</td></tr>
+			</tbody>
+		</table>
+		<p>
+            Setelah mereka membuat game rpg berjudul Super Mario RPG, mereka absen dari spinoff rpg sampai pada tahun 2003.<br>
+            Dengan studio bernama AlphaDream, mereka membuat spinoff dengan banyak karakter baru bernama Mario & Luigi.<br>
+            Kali ini, saya memainkan versi GBA (Bukan yang Remake).<br>
+        </p>
+        <h3>Cerita</h3>
+        <p>
+            Untuk permainan ini, ceritanya tidak terlalu serius, namun cukup fun untuk diikuti.<br>
+            Yang jelas, karakter-karakter yang anda kamu ketahui tidak terlalu berbeda.<br>
+            Karakter-karakter baru sangat fresh dan menambah suatu komedi (meskipun beberapa ada yang tipikal mario luigi clone).<br>
+
+        </p>
+        <h3>Permainan</h3>
+        <p>
+            <img src="https://upload.wikimedia.org/wikipedia/en/0/03/MarioAndLuigiSuperstarSagaGBABattle.png" alt="battle"><br>
+            Ini adalah turn-based rpg yang tidak biasa, dimana berhasil/tidaknya serangan bergantung pada input anda.<br>
+            Monster akan terlihat dan bisa menyerang anda jika berada dalam rangenya, pada saat bersamaan anda juga bisa menyerangnya dengan palu (didapatkan suatu saat) dan melompat (ala mario bros.)<br>
+            Ada 3 jenis awalan sebelum memasuki battle sequence;<br>
+            - anda berhasil menyerang dia sebelum musuh menyerang anda > jika palu, musuh anda (yang tidak immune) akan pusing, dan jika melompat, maka jatuh ke musuh (tergantung dengan situasi, bisa menguntungkan, bisa tidak)<br>
+            - anda tidak menyerang > battle sequence dengan normal<br>
+            - anda diserang (saat musuh menyentuh belakang anda) > anda akan stunned, luigi lebih besar kemungkinan masih stun selama 1-2 ronde<br>
+            Anda bermain dengan 2 karakter (Mario dan Luigi), dengan 2 tombol yang berbeda.<br>
+            Terlihat membingungkan, tetapi saat bermain tidak akan susah, bahkan unik.<br>
+            Anda mempunyai 3 pilihan saat battle sequence;<br>
+            SOLO : Menyerang sendiri<br>
+            BROS : Menyerang bersama (menghabiskan BP)<br>
+            ITEM : Menggunakan item<br>
+            RUN : Lari tanpa mendapatkan apa-apa (kabur)<br>
+            saat menggunakan BROS attack, anda diharuskan untuk menginput secara konsekutif supaya berhasil, dan ini satu-satunya serangan damage tinggi.<br>
+            Saat defence sequence, anda bisa menghindar bahkan counter serangan musuh dengan melompat atau memukul (akan dipilih sendiri)<br><br>
+
+            Saat menjelajah, penggunaan lompat dan palu juga digunakan untuk menyelesaikan puzzle dan lain.<br>
+            Menurutku, dengan battle yang dinamis dan penjelajahan yang tetap memerlukan platforming kecil (namun kecil, ala mario bros), sangat menyenangkan<br>
+            Dan lebih baik lagi, anda bisa menghindar banyak musuh kecil, karena anda tidak perlu exp yang banyak untuk melawan boss besar (jika anda bisa menghindari seranganya dengan baik).<br>
+        </p>
+        <h3>Aestetis</h3>
+        <p>
+            <img src="https://r.mprd.se/media/images/44515-Mario_And_Luigi_Superstar_Saga_(U)(Rising_Sun)-13.png" alt="boss">
+            <img src="https://vignette.wikia.nocookie.net/mario/images/a/a5/Little_Toad_SS.PNG" alt="overworld">
+            <img src="https://www.retrogames.cc/screenshot/43/q/26855_2055ac56d91333de4830cc6298f6ba9036d2149b.png" alt="bowser"><br>
+            Aestetis pada awal umur GBA cukup reasonable dan sangat cerah (karena GBA layarnya sangat redup).<br>
+            Yang kusuka dari seri ini adalah saat AlphaDream mencoba pertama kali untuk menggambar sesuatu ekspresif.<br>
+            Hasilnya sangat lucu dan unik. Tidak terasa kaku.<br>
+            Setiap stage juga terasa berbeda.<br>
+        </p>
+        <h3>Suara</h3>
+        <p>
+            Dengan keterbatasan hardware, pilihan instrumen yang dia pilih bagus.<br>
+            Anda bisa merasa bersemangat dengan lagu battle ini.<br>
+            <iframe width="478" height="269" src="https://www.youtube.com/embed/VOC0ig2Rk1g?list=PL6akIKaXBeU1qtP8Cncj6mHt0gIXLqqb-" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>
+        </p>
+		
+		<table  class="table table-striped">
+			<thead class="thead-dark">
+				<tr>
+					<th colspan="3">Verdict</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td rowspan="5"><h1>8</h1></td>
+				</tr>
+                <tr><td>Story</td><td>8/10</td></tr>
+                <tr><td>Gameplay</td><td>8/10</td></tr>
+				<tr><td>Graphics</td><td>8/10</td></tr>
+                <tr><td>Sounds</td><td>8/10</td></tr>
+			</tbody>
+		</table>
+		<br>
+	</div>
+	<br>
+	<div class="container">
+		<h3>Komentar</h3>
+    <div class="row">
+		<?php 
+			$u_id = $_SESSION['id'];
+		
+			include('..\comment_fun.php');
+			include('..\encrypt_decrypt.php');
+			if (($num_comments  = mysqli_num_rows($comment)) > 0){
+				echo '<div class=col-sm-12><h4>Ada '."$num_comments".' komentar.</h4></div>';
+			while($display_com = mysqli_fetch_assoc($comment)){?>
+        <div class="col-sm-12">
+            <div class="panel panel-white post">
+                <div class="post-heading">
+                        <div class="title h5">
+                            <b><?php echo decrypt($display_com['u_name'])?></b> made a post.
+                        </div>
+                        <h6 class="text-muted time"><?php echo $display_com['c_time']?></h6>
+                </div> 
+                <div class="post-description"> 
+                    <p><?php echo $display_com['c_contents']?></p>
+                </div>
+            </div>
+		</div>
+			<?php }} 
+			else echo '<div class="col-sm-12"><h4>Tidak ada komentar</h4></div>';?>
+
+	</div>
+	<?php include '../comment_form.php';?>
+		<br>
+	</div>
+</body>
+</html>
---
+++ simulation/sims1.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+
+<html lang="id" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="../CSS.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+    <link rel="icon" href="logo.ico">
+	<title>NGR - Nice Game Review</title>
+ <style>
+
+	img{
+		width:250px;
+		height:150px;
+	}
+
+</style>
+<body>
+	<div class="container-top">
+		<?php include '../header.php';?>
+	</div>
+
+
+	<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
+		<?php include '../navbar.php';?>
+	</nav>
+	<div class="container">	
+			<br>
+		<table class="table table-striped">
+			<thead class="thead-dark">
+				<tr>
+					<th colspan="3">The Sims</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td rowspan="5"><img src="https://upload.wikimedia.org/wikipedia/en/6/63/MarioAndLuigiSuperstarSagaGBACoverArtUS.jpg" alt="Superstar Saga" /></a></td>
+				</tr>
+				<tr><td>Developer</td><td>Maxis</td></tr>
+				<tr><td>Publisher</td><td>Electronic Arts</td></tr>
+				<tr><td>Platform</td><td>PC</td></tr>
+				<tr><td>Release Date</td><td>January 31 2001</td></tr>
+			</tbody>
+		</table>
+		<p>
+            Setelah mengerjakan beberapa seri untuk SimCity, mereka beranjak menuju sesuatu yang berbeda.<br>
+            Will Wright membuat simulasi yang sangat berbeda, yaitu simulasi kehidupan manusia.<br>
+            
+        </p>
+        <h3>Permainan</h3>
+        <p>
+            <img src="http://www.mobygames.com/images/shots/l/4284-the-sims-windows-screenshot-create-a-sim-punk-grannys.jpg" alt="charas"><br>
+            Dimulai dengan pembuatan sim (manusia), anda membuat penampakan karakter dan sifat mereka.<br>
+            Anda diberi kebebasan untuk membuat berapa sim beserta dengan hubungannya.<br><br>
+            <img src="https://i2.wp.com/www.platinumsimmers.com/wp-content/uploads/2014/02/BELLAAAAAAA.jpg" alt="interact"><br>
+            Karena ini simulasi, anda diberikan kebebasan pula untuk mengatur kehidupan mereka, membangun rumah, dsb.<br>
+            Tetapi, jika anda ingin mereka bahagia, anda harus memenuhi kebutuhan sehari-hari, kerja, sosial, dsb.<br>
+            Mungkin ini kurang variatif dan primitif, tetapi ingat ini permainan tahun 2000.<br>
+            Menurutku, permainan ini cukup bervariatif dan bisa membuat anda bermain selama berjam-jam seperti SimCity, namun lebih personal dan mudah (tidak mengatur kota-kota).<br>
+        </p>
+        <h3>Aestetis</h3>
+        <p>
+            <img src="http://theplumbobsite.files.wordpress.com/2015/06/sims1-10.jpg" alt="pet">
+            <img src="https://d26bwjyd9l0e3m.cloudfront.net/wp-content/uploads/2014/11/The-Sims-Screenshot-6.jpg" alt="party">
+            <img src="https://www.oldpcgaming.net/wp-content/gallery/the-sims/23_1.jpg" alt="paint"><br>
+            Menggunakan 3D model untuk karakter dan pre-rendered untuk lainnya, aestetisnya sangat "millenial awal".<br>
+            Ini untuk mengurangi resource sehingga spec standar dari tahun 2000 tidak perlu berat-berat.<br>
+            Mungkin kurang bagus, tetapi cukup ngeblend 3D dan pre-rendered.<br>
+        </p>
+        <h3>Suara</h3>
+        <p>
+            Lagu bersemangat, lagu santai, lainnya, semua lagu terdengar sangat nyaman.<br>
+            <iframe width="478" height="269" src="https://www.youtube.com/embed/a_j2PpZKIJw?list=PLFD0B65AF1A236E8C" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>
+        </p>
+		
+		<table  class="table table-striped">
+			<thead class="thead-dark">
+				<tr>
+					<th colspan="3">Verdict</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td rowspan="4"><h1>7</h1></td>
+				</tr>
+                <tr><td>Gameplay</td><td>8/10</td></tr>
+				<tr><td>Graphics</td><td>7/10</td></tr>
+                <tr><td>Sounds</td><td>8/10</td></tr>
+			</tbody>
+		</table>
+		<br>
+	</div>
+	<br>
+	<div class="container">
+		<h3>Komentar</h3>
+    <div class="row">
+		<?php 
+			$u_id = $_SESSION['id'];
+		
+			include('..\comment_fun.php');
+			include('..\encrypt_decrypt.php');
+			if (($num_comments  = mysqli_num_rows($comment)) > 0){
+				echo '<div class=col-sm-12><h4>Ada '."$num_comments".' komentar.</h4></div>';
+			while($display_com = mysqli_fetch_assoc($comment)){?>
+        <div class="col-sm-12">
+            <div class="panel panel-white post">
+                <div class="post-heading">
+                        <div class="title h5">
+                            <b><?php echo decrypt($display_com['u_name'])?></b> made a post.
+                        </div>
+                        <h6 class="text-muted time"><?php echo $display_com['c_time']?></h6>
+                </div> 
+                <div class="post-description"> 
+                    <p><?php echo $display_com['c_contents']?></p>
+                </div>
+            </div>
+		</div>
+			<?php }} 
+			else echo '<div class="col-sm-12"><h4>Tidak ada komentar</h4></div>';?>
+
+	</div>
+	<?php include '../comment_form.php';?>
+		<br>
+	</div>
+</body>
+</html>
