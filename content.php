e15ac48dcbce92e4a202ff5abc55190e4a287a0c
Mhd.Fadly Hasan <37266671+mhdfadlyhasan@users.noreply.github.com>
2018-12-20 06:30:31 +0700

    revised

--- content.php
+++ content.php
@@ -11,8 +11,8 @@ if($Type == "html")
 {
 move_uploaded_file($_FILES["content"]["tmp_name"], $target_file);
 
-$sql = "INSERT INTO articles (a_img_link, a_genre, a_name)
-VALUES ('".$_POST["photolink"]."', '".$_POST["genre"]."', '".$_POST["gamename"]."')";
+$sql = "INSERT INTO articles (a_img_link, a_genre, a_link, a_name)
+VALUES ('".$_POST["photolink"]."', '".$_POST["genre"]."','../../tugas7-fp-ngr/".$_POST["genre"]."/".basename($_FILES["content"]["name"])."','".$_POST["gamename"]."')";
 if ($conn->query($sql) === TRUE) {
 	echo $_POST["gamename"]." has been uploaded";
 }
